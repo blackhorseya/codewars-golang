52874cd7c993628ff548b8483b6b9270ebe5c7f1
Sean Cheng <blackhorseya@gmail.com>
2020-11-26 17:27:57 +0800

    refactor: modified Maximum subarray sum
    - using Dynamic Programming improve performance

--- kata/maximumSubarraySum.go
+++ kata/maximumSubarraySum.go
@@ -5,20 +5,20 @@ func MaximumSubarraySum(numbers []int) int {
 		return 0
 	}
 
-	ret := numbers[0]
-	for i := 0; i < len(numbers); i++ {
-		sum := numbers[i]
-		for j := i + 1; j < len(numbers); j++ {
-			sum += numbers[j]
-			if sum > ret {
-				ret = sum
-			}
-		}
-	}
-
-	if ret < 0 {
-		return 0
+	ret, sum := numbers[0], 0
+	for _, number := range numbers {
+		sum += number
+		sum = max(0, sum)
+		ret = max(ret, sum)
 	}
 
 	return ret
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+
+	return b
 }
