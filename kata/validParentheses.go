bc8fac9c04da6818d8a389c047ecb03280574bea
Sean Cheng <blackhorseya@gmail.com>
2020-11-28 00:40:24 +0800

    refactor: modified ValidParentheses

--- kata/validParentheses.go
+++ kata/validParentheses.go
@@ -1,21 +1,17 @@
 package kata
 
 func ValidParentheses(parens string) bool {
-	var list []string
+	count := 0
 	for _, letter := range parens {
 		if letter == '(' {
-			list = append(list, string(letter))
+			count++
 		} else {
-			if len(list) == 0 {
+			count--
+			if count < 0 {
 				return false
 			}
-			list = append(list[:len(list)-1], list[len(list)-1+1:]...)
 		}
 	}
 
-	if len(list) != 0 {
-		return false
-	}
-
-	return true
+	return count == 0
 }
