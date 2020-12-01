3ad7b90387cc7c289aa0659b38c512a4348430fa
Sean Cheng <blackhorseya@gmail.com>
2020-12-01 08:54:25 +0800

    refactor: modified Complementary DNA

--- kata/complementaryDNA.go
+++ kata/complementaryDNA.go
@@ -1,21 +1,13 @@
 package kata
 
-func DNAStrand(dna string) string {
-	ret := ""
-	for _, letter := range dna {
-		switch letter {
-		case 'A':
-			ret += "T"
-		case 'T':
-			ret += "A"
-		case 'C':
-			ret += "G"
-		case 'G':
-			ret += "C"
-		default:
-			ret += string(letter)
-		}
-	}
+import "strings"
 
-	return ret
+var replacer *strings.Replacer = strings.NewReplacer("A", "T",
+	"T", "A",
+	"C", "G",
+	"G", "C",
+)
+
+func DNAStrand(dna string) string {
+	return replacer.Replace(dna)
 }
